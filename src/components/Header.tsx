2ef6a16fbd29bf45e9af1439a0c25e756844a8a6
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-02-20 11:50:44 +0900

    new!

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/mobile/Index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileIndex = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default MobileIndex
