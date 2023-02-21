183ccdd221444d27f80a6e3a89b1795fc2153e50
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-02-21 16:47:50 +0900

    qrcode

--- src/components/QRCodes.tsx
+++ src/components/QRCodes.tsx
@@ -1,9 +1,14 @@
 import styles from '../styles/index.module.css'
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
+import {  BrowserView,  MobileView } from "react-device-detect";
 
+type linkType = {
+  kakaoLink: string
+  tossLink: string
+}
 
-const QRCodes = () => {
+const QRCodes = ({kakaoLink, tossLink}: linkType) => {
   const [kakao, setKakao] = useState(false)
   const [toss, setToss] = useState(false)
   const updateState = (type: number) => {
@@ -20,13 +25,12 @@ const QRCodes = () => {
       <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvnI4L%2FbtrZ7FWE7MW%2Fnh5j29QL2FPBGNKUgttRH0%2Fimg.png" alt="카카오QR코드" />
     </div>
   )
-  
   const TossIMG = () => (
     <div className={styles.QRImgWrap} onClick={()=>setToss(false)}>
       <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbXD82d%2FbtrZ5jGQqlR%2FWN2kBCfdZHKF0DBaEwSebK%2Fimg.jpg" alt="토스QR코드" />
     </div>
   )
-  
+
   return (
     <div className={styles.qrWrap}>
       <Button className={styles.qrButtons} onClick={()=> updateState(1)}  size="lg" variant="warning" active>
@@ -35,8 +39,14 @@ const QRCodes = () => {
       <Button className={styles.qrButtons} onClick={()=>updateState(2)}  size="lg" variant="primary" active>
         Toss - Buy me a Coffee ☕
       </Button>
-      { kakao ? <KakaoIMG /> : '' }
-      { toss ? <TossIMG /> : '' }
+      <BrowserView>
+        { kakao ? <KakaoIMG /> : '' }
+        { toss ? <TossIMG /> : '' }
+      </BrowserView>
+      <MobileView>
+        {kakao ? window.location.href=`${kakaoLink}` : ''}
+        {toss ? window.location.href=`${tossLink}` : ''}
+      </MobileView>
     </div>
   )
 }
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,9 +7,11 @@ type myInfoType = {
   myName: string; //이름작성하기
   myType: string;
   workFor: workForType[]; //현재 관심/일 하고 있는 언어 작성 
-  tistoryLink: string
-  githubLink: string
-  notionLink: string
+  tistoryLink: string | ''
+  githubLink: string | ''
+  notionLink: string | ''
+  kakaoLink: string
+  tossLink: string
 }
 
 const myInfo: myInfoType = {
@@ -18,14 +20,16 @@ const myInfo: myInfoType = {
   workFor: ['Javascript','React.js', 'Vue.js'],
   tistoryLink: 'https://code-hoon.tistory.com',
   githubLink: 'https://github.com/Hoon-Hub',
-  notionLink: 'https://k-sanghoon.notion.site/31144445ed6b42be9f1e0afee2245a46'
+  notionLink: 'https://k-sanghoon.notion.site/31144445ed6b42be9f1e0afee2245a46',
+  kakaoLink: 'https://qr.kakaopay.com/Ej7jPN3lW',
+  tossLink: 'https://toss.me/ksanghoon'
 }
 
 const MobileIndex = () => {
   return (
     <div className={styles.mainContentsWrap }>
       <Content myName={myInfo.myName} myType={myInfo.myType} workFor={myInfo.workFor} tistoryLink={myInfo.tistoryLink} githubLink={myInfo.githubLink} notionLink={myInfo.notionLink}/>
-      <QRCodes />
+      <QRCodes kakaoLink={myInfo.kakaoLink} tossLink={myInfo.tossLink} />
     </div>
   )
 }
